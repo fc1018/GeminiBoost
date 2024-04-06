b39b1637554d09f26039c1d342a338c55977977a
Felix <mail@felixchan.net>
2024-04-06 10:22:32 -0700

    Increased function delay and added error handling

--- boost.js
+++ boost.js
@@ -1,12 +1,19 @@
 async function queryEntry(question) {
-    //Find text field and insert query
+    // Find text field and insert query
     let input = document.querySelector("rich-textarea > * > p");
-    input.textContent = question;
-    //Find button and click
-    setTimeout(() => {
-        let submit = document.querySelector("input-area-v2 > div > div > div > div > div > button");
-        submit.click();
-    }, 50);
+    if (input) {
+        input.textContent = question;
+
+        // Find button and click
+        setTimeout(() => {
+            let submit = document.querySelector("input-area-v2 > div > div > div > div > div > button");
+            if (submit) {
+                submit.click();
+            }
+        }, 50);
+    } else {
+        console.error("Could not find input element");
+    }
 }
 
 async function fillQuery() {
@@ -15,12 +22,14 @@ async function fillQuery() {
         get: (searchParams, prop) => searchParams.get(prop),
     });
     let question = params.question;
+    console.log("Found question:", question);
     if (!question) {
+        console.error("Could not find question");
         return;
     }
     //Document load status check
     window.onload = (event) => {
-        setTimeout(() => queryEntry(question), 100)
+        setTimeout(() => queryEntry(question), 350)
     };
 }
 fillQuery();
